e7ae5728e293f1b7ccb8aad6d84afcfd685bdfc8
wb-ztc537764 <wb-ztc537764@autonavi.com>
2019-07-09 16:38:30 +0800

    修改登录超时返回登录页面

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,20 +15,14 @@ const service = axios.create({
 // request interceptor
 service.interceptors.request.use(
   config => {
-    // do something before request is sent
     console.log("config -- ", config)
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
       config.headers['Authorization'] = getToken()
     }
-    // console.log("config --- >", config)
     return config
   },
   error => {
     console.log("process.env.VUE_APP_BASE_API -- ", process.env.VUE_APP_BASE_API)
-    // do something with request error
     console.log(error) // for debug
     return Promise.reject(error)
   }
@@ -36,48 +30,11 @@ service.interceptors.request.use(
 
 // response interceptor
 service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
-
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
   response => {
     const res = response.data
-    // console.log("res --- >", res);
     return res;
-    // if the custom code is not 20000, it is judged as an error.
-    // if (res.code !== 20000) {
-    //   Message({
-    //     message: res.message || 'error',
-    //     type: 'error',
-    //     duration: 5 * 1000
-    //   })
-
-    //   // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-    //   if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-    //     // to re-login
-    //     MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-    //       confirmButtonText: 'Re-Login',
-    //       cancelButtonText: 'Cancel',
-    //       type: 'warning'
-    //     }).then(() => {
-    //       store.dispatch('user/resetToken').then(() => {
-    //         location.reload()
-    //       })
-    //     })
-    //   }
-    //   return Promise.reject(res.message || 'error')
-    // } else {
-    //   return res
-    // }
-  },
+Í  },
   error => {
-    console.log("err -- ", error);
     if (error.response) {
       if (error.response.status) {
         switch (error.response.status) {
@@ -92,10 +49,12 @@ service.interceptors.response.use(
             }
             break;
           case 403:
+            console.log("dddd")
             Message({
               message: '登录过期，请重新登录' || 'error',
               type: 'error',
               onClose: function () {
+                store.dispatch('user/logout')
                 router.replace({
                   path: '/login',
                   query: {
